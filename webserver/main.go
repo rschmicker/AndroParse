f44aced57a4167fc473d1f2a7febea449eae3df9
rschmicker <rhschmicker@gmail.com>
2018-07-28 17:04:57 -0400

    change default port

--- webserver/main.go
+++ webserver/main.go
@@ -37,7 +37,7 @@ func main() {
 		},
 	}
 	srv := &http.Server{
-		Addr:         ":443",
+		Addr:         ":8080",
 		Handler:      mux,
 		TLSConfig:    cfg,
 		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
